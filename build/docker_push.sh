db9ebe4c694541b35a938819ca77eeb20c5dfd53
Josh Smith <smithandweb+github@gmail.com>
2021-06-09 01:10:16 -0300

    change short sha variable name and travis build

--- build/docker_push.sh
+++ build/docker_push.sh
@@ -10,12 +10,12 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the main branch
   if [ "$TRAVIS_BRANCH" == "dev" ]; then
 
-    DD_VERSION=`git log --pretty=format:'%h' -n 1`
+    SHORTSHA=`git log --pretty=format:'%h' -n 1`
 
-    docker tag koi_node:dev "public.ecr.aws/r3r0i7b9/koi_node:dev-$DD_VERSION"
-    docker push "public.ecr.aws/r3r0i7b9/koi_node:dev-$DD_VERSION"
+    docker tag koi_node:dev "public.ecr.aws/r3r0i7b9/koi_node:dev-$SHORTSHA"
+    docker push "public.ecr.aws/r3r0i7b9/koi_node:dev-$SHORTSHA"
 
-    echo "Pushed koi_node:dev-$DD_VERSION"
+    echo "Pushed koi_node:dev-$SHORTSHA"
 
     ./deploy.sh
 
