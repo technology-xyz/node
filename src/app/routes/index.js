460c83b24781bb425a3bd16ee7bb0eae062194d7
Andre Vallestero <andrevallestero@gmail.com>
2021-06-10 11:40:43 -0400

    Change state-predicted to predicted

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
   app.get("/nodes", nodes);
   app.get("/state/current", getCurrentState);
   app.get("/state/current-cached", getCurrentStateCached);
-  app.get("/state/current-state-predicted", getCurrentStatePredicted);
+  app.get("/state/current-predicted", getCurrentStatePredicted);
   app.get("/state/top-content-predicted", getTopContentPredicted);
   app.get("/state/nft", cache, getNFTState);
   app.post("/handle-nft-upload", handleNFTUpload);
