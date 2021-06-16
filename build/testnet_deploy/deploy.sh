5f154674bf142eb817f89892679165d227bc37e1
Josh Smith <smithandweb+github@gmail.com>
2021-06-16 00:11:34 -0300

    updated the deploy script

--- build/testnet_deploy/deploy.sh
+++ build/testnet_deploy/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Deploys an 8 pod stack of koi-nodes based on the values.*.yaml that are in this directory.
+# You can pass in a docker image tag as the first argument, and an AWS profile flag as a second argument. Both are optional.
 
 nodes=(
     "node1"
@@ -11,18 +14,26 @@ nodes=(
     "node8"
 )
 
-if [[ -n "$1" ]]; then
-  export AWS_PROFILE=$1
+if [ "$TRAVIS_BRANCH" == "dev" ]; then
+
+    SHORTSHA=`git log --pretty=format:'%h' -n 1`
+    TAG="--set image.tag=$SHORTSHA"
+
 fi
 
-tag=""
+if [ -n "$1" ]; then
+
+    TAG="--set image.tag=$1"
+
+fi
+
 if [[ -n "$2" ]]; then
-    tag="--set image.tag=$2"
+  export AWS_PROFILE=$2
 fi
 
 for node in ${nodes[@]}; do
     (cd koi-node && \
     sops -d ../values.$node.yaml > ../values.$node.dec.yaml && \
-    helm upgrade --install koi-$node . -f ../values.$node.dec.yaml `echo $tag` \
+    helm upgrade --install koi-$node . -f ../values.$node.dec.yaml `echo $TAG` \
     )
 done
