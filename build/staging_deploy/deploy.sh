005cfc32c4c6f5f1a6cc150a3634a59b3b35f15f
Josh Smith <smithandweb+github@gmail.com>
2021-06-23 01:12:39 -0300

    staging k8s context

--- build/staging_deploy/deploy.sh
+++ build/staging_deploy/deploy.sh
@@ -22,7 +22,7 @@ if [ "$TRAVIS_BRANCH" == "main" ]; then
     # This is not a pull request in travis. Configure kubectl, eksctl
     if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-        aws eks update-kubeconfig --name koi --region us-west-2
+        aws eks update-kubeconfig --name koi-staging --region us-west-2
 
         export HELM_EXPERIMENTAL_OCI=1
         helm chart pull public.ecr.aws/r3r0i7b9/koi_node_helm:latest
