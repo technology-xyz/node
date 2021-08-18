269e91a88515bcc39c456f97b570a0ced3d71af0
Josh Smith <smithandweb+github@gmail.com>
2021-08-17 21:42:06 -0300

    update staging deploy

--- build/staging_deploy/deploy.sh
+++ build/staging_deploy/deploy.sh
@@ -15,22 +15,22 @@ nodes=(
 
 
 
-if [ "$TRAVIS_BRANCH" == "main" ]; then
+if [ "$TRAVIS_BRANCH" == "dev" ]; then
 
     cd $TRAVIS_BUILD_DIR/build/staging_deploy
 
     # This is not a pull request in travis. Configure kubectl, eksctl
     if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-        aws eks update-kubeconfig --name koi-staging --region us-west-2
+        aws eks update-kubeconfig --name koi --region us-west-2
 
         export HELM_EXPERIMENTAL_OCI=1
         helm chart pull public.ecr.aws/r3r0i7b9/koi_node_helm:latest
         helm chart export public.ecr.aws/r3r0i7b9/koi_node_helm:latest
 
         for node in ${nodes[@]}; do
-            sops -d values.$node.yaml > values.$node.dec.yaml
-            helm upgrade --install koi-$node ./koi-node -n koi -f values.$node.dec.yaml
+            sops -d ./$node/secrets.yaml > ./$node/secrets.dec.yaml
+            helm upgrade --install koi-$node ./koi-node -n koi -f ./$node/secrets.dec.yaml -f ./$node/values.yaml
         done
 
     fi
