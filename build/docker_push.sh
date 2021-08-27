adb7cb7758919e0d89828667eb3f4da3f67149a2
Josh Smith <smithandweb+github@gmail.com>
2021-08-27 13:25:04 -0300

    updating deploys

--- build/docker_push.sh
+++ build/docker_push.sh
@@ -5,45 +5,31 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   pip install awscli
   export PATH=$PATH:$HOME/.local/bin
+  SHORTSHA=`git log --pretty=format:'%h' -n 1`
 
   aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/r3r0i7b9
   # Push only if we're testing the main branch
   if [ "$TRAVIS_BRANCH" == "dev" ]; then
-
-    SHORTSHA=`git log --pretty=format:'%h' -n 1`
 
     docker tag koi_node:dev "public.ecr.aws/r3r0i7b9/koi_node:dev-$SHORTSHA"
     docker push "public.ecr.aws/r3r0i7b9/koi_node:dev-$SHORTSHA"
 
     echo "Pushed koi_node:dev-$SHORTSHA"
 
-    ./build/testnet_deploy/deploy.sh
+    ./build/dev_deploy/deploy.sh
 
   fi
 
   if [ "$TRAVIS_BRANCH" == "main" ]; then
 
-    docker tag koi_node:dev public.ecr.aws/r3r0i7b9/koi_node:main
+    docker tag koi_node:dev "public.ecr.aws/r3r0i7b9/koi_node:main-$SHORTSHA"
     docker tag koi_node:dev public.ecr.aws/r3r0i7b9/koi_node:latest
-    docker push "public.ecr.aws/r3r0i7b9/koi_node:main"
+    docker push "public.ecr.aws/r3r0i7b9/koi_node:main-$SHORTSHA"
     docker push "public.ecr.aws/r3r0i7b9/koi_node:latest"
 
-    echo "Pushed koi_node:main and koi_node:latest"
+    echo "Pushed koi_node:main-$SHORTSHA and koi_node:latest"
 
-    ./build/staging_deploy/deploy.sh
-
-  fi
-
-  if [ "$TRAVIS_BRANCH" == "kohaku" ]; then
-
-    SHORTSHA=`git log --pretty=format:'%h' -n 1`
-
-    docker tag koi_node:dev "public.ecr.aws/r3r0i7b9/koi_node:kohaku-$SHORTSHA"
-    docker push "public.ecr.aws/r3r0i7b9/koi_node:kohaku-$SHORTSHA"
-
-    echo "Pushed koi_node:kohaku-$SHORTSHA"
-
-    ./build/kohaku_deploy/deploy.sh
+    ./build/prod_deploy/deploy.sh
 
   fi
 
